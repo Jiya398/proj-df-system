3b9f5740cd9eb92e909310bcae6ccd874df9c888
Jiya398 <Julietaa.b.sayco@gmail.com>
2024-05-21 21:39:18 +0800

    Add product resource with forms and tables

--- app/Filament/Resources/ProductResource.php
+++ app/Filament/Resources/ProductResource.php
@@ -5,12 +5,20 @@ namespace App\Filament\Resources;
 use App\Filament\Resources\ProductResource\Pages;
 use App\Filament\Resources\ProductResource\RelationManagers;
 use App\Models\Product;
+use App\Models\Category;
 use Filament\Forms;
+use Filament\Forms\Components\FileUpload;
 use Filament\Forms\Form;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\TextInput;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
+use Filament\Tables\Columns\ImageColumn;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
 
 
 class ProductResource extends Resource
@@ -21,11 +29,31 @@ class ProductResource extends Resource
 
     protected static ?string $navigationGroup = 'Shop';
 
+    protected static ?int $navigationSort = 0;
+
     public static function form(Form $form): Form
     {
         return $form
             ->schema([
-                //
+                Section::make('User Information')
+                    ->collapsible()
+                    ->columns(6)
+                    ->schema([
+                        Forms\Components\TextInput::make('name')
+                            ->required()
+                            ->columnSpan(3)
+                            ->autofocus()
+                            ->live(onBlur: true),
+
+                        Forms\Components\TextInput::make('slug')
+                            ->required()
+                            ->columnSpan(3)
+                            ->dehydrated(),
+
+                        FileUpload::make('featured_image')
+                            ->label('Featured Image')
+                            ->columnSpan(3),
+                    ])
             ]);
     }
 
@@ -33,13 +61,34 @@ class ProductResource extends Resource
     {
         return $table
             ->columns([
-                //
+                Tables\Columns\TextColumn::make('name')
+                    ->label('Name')
+                    ->sortable()
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('slug')
+                    ->sortable()
+                    ->searchable(),
+                Tables\Columns\ImageColumn::make('featured_image')
+                    ->label('Image')
+                    ->sortable()
+                    ->searchable(),
             ])
             ->filters([
-                Tables\Filters\TrashedFilter::make(),
+                Tables\Filters\TernaryFilter::make('is_visible')
+                    ->label('Visibility')
+                    ->trueLabel('Only visible products')
+                    ->falseLabel('Only hidden products')
+                    ->native(false)
+                    ->boolean(),
             ])
             ->actions([
-                Tables\Actions\EditAction::make(),
+                Tables\Actions\ActionGroup::make([
+                    Tables\Actions\ActionGroup::make([
+                        Tables\Actions\ViewAction::make(),
+                        Tables\Actions\EditAction::make(),
+                    ])->dropdown(false),
+                    Tables\Actions\DeleteAction::make(),
+                ]),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
